c8766807b64debc7353b4557e00dc48d5c737b9d
LAPTOP123456789\User <yuzhongng@gmail.com>
2023-03-18 23:51:49 +0800

    Add HUD UI and portal to change scene

--- Assets/Script/UIFollowCamera.cs
+++ Assets/Script/UIFollowCamera.cs
@@ -1,28 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class UIFollowCamera : MonoBehaviour
 {
     public GameObject camera;
-    public GameObject hud;
+    //public GameObject hud;
+    //public Transform target;
 
     private Vector3 offset;
-    //private Vector3 angle;
+    private float angleDiff;
+    private float currZ;
+    private float newZ;
+    private float currX;
+
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - camera.transform.position;
-       // angle = transform.rotation;
+        // angleDiff = Quaternion.Angle(gameObject.transform.rotation, camera.rotation);
+        angleDiff = Quaternion.Angle(transform.rotation, camera.transform.rotation);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {   
-        // if (transform.rotation.compare(angle)) {
-        //     transform.position += offset;
-        // }
-        transform.position = camera.transform.position + offset;
-        transform.rotation = camera.transform.rotation;
+        //angleDiff = Quaternion.Angle(transform.rotation, camera.transform.rotation);
+        if (angleDiff < 0) {
+            currZ = offset.z;
+            newZ = currZ - currZ* (float) (Math.Cos(angleDiff));
+            currX = currZ* (float) (Math.Sin(angleDiff));
+            transform.position = new Vector3(transform.position.x - currX, 0.0f, currZ - newZ);
+            transform.rotation = camera.transform.rotation;
+        }  else if (angleDiff == 0) {
+            transform.position = camera.transform.position + offset;
+            transform.rotation = camera.transform.rotation;
+        } else if (angleDiff > 0) {
+            currZ = offset.z;
+            newZ = currZ - currZ* (float) (Math.Cos(angleDiff));
+            currX = currZ* (float) (Math.Sin(angleDiff));
+            transform.position = new Vector3(transform.position.x + currX, 0.0f, currZ - newZ);
+            transform.rotation = camera.transform.rotation;
+        } else {}
+        // transform.position = camera.transform.position + offset;
+        // transform.RotateAround(camera.transform.position, Vector3.up, angleDiff);
     }
 }
