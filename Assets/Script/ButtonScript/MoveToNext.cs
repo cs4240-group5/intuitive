54b0bb297eb05409ddc6dcccfc83d9df53b0a24a
sallysohyun <42793160+sallysohyun@users.noreply.github.com>
2023-03-20 13:54:47 +0900

    Created Scene

---
+++ Assets/Script/ButtonScript/MoveToNext.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+public class MoveToNext : MonoBehaviour
+{
+    public GameObject nextButton;
+    public GameObject currentboard;
+    public GameObject currentText;
+    public GameObject newboard;
+    public GameObject newText;
+    public Button openButton;
+    public AudioSource audioPlayer;
+
+    public void clickStart()
+    {
+        currentboard.SetActive(false);
+        currentText.SetActive(false);
+        newboard.SetActive(true);
+        newText.SetActive(true);
+        openButton.interactable = false;
+        audioPlayer.Play();
+    }
+}
