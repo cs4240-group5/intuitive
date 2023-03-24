783eb1622d78bee7de2ac7e6833a13bb8d698623
NUSSTU\e0544359 <yuzhongng@gmail.com>
2023-03-24 10:29:37 +0800

    update stuff

--- Assets/Script/GetOptions.cs
+++ Assets/Script/GetOptions.cs
@@ -26,23 +26,30 @@ public class GetOptions : MonoBehaviour
         // Save the current height of the panel and the starting value of the slider
         slider.minValue = 0;
         slider.maxValue = buttonObjects.Length;
-        slider.value = buttonObjects.Length;    
+        slider.value = 0;
+
+        // disable second text object.
+        textInst_2.transform.localScale = Vector2.zero;
+        for (int i = 0; i < buttonObjects.Length; i++)
+        {
+            buttonObjects[i].transform.localScale = Vector2.zero;
+        }
     }
 
     void Update()
     {
         // Calculate the percentage change in the slider value
         
-        if(slider.value != buttonObjects.Length)
+        if(slider.value != 0)
         {
             OnSliderValueChanged();
         }
         int currentValue = (int)Math.Floor(slider.value);
         for (int i = 0; i < currentValue; i++) {
-            buttonObjects[i].SetActive(true);
+            buttonObjects[i].transform.LeanScale(Vector2.one, 0.2f);
         }
         for (int i = currentValue; i < slider.maxValue; i++) {
-            buttonObjects[i].SetActive(false);
+            buttonObjects[i].LeanScale(Vector2.zero, .2f).setEaseInBack();
         }
 
         boardtext.text = "Current Option Count: " + currentValue;
@@ -51,8 +58,8 @@ public class GetOptions : MonoBehaviour
 
     void OnSliderValueChanged()
     {
-        textInst_1.SetActive(false);
-        textInst_2.SetActive(true);
+        textInst_1.transform.localScale = Vector2.zero;
+        textInst_2.transform.LeanScale(Vector2.one, 0.2f);
     }
 
 }
