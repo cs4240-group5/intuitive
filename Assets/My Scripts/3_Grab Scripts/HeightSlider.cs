a9ec1a1d98c581e41f8590c006718f44714a680d
Zhan Hong <choongorilla@gmail.com>
2023-04-03 12:56:49 +0800

    Finalise slider label
    If have time, I can change it to buttons. But this is fine for now.

--- Assets/My Scripts/3_Grab Scripts/HeightSlider.cs
+++ Assets/My Scripts/3_Grab Scripts/HeightSlider.cs
@@ -8,12 +8,8 @@ public class HeightSlider : MonoBehaviour
     public Slider distanceSlider; 
     private Vector3 originalPos;
     private double distance;
-    public GameObject refObj;
-    
-    // private double minRange = 1.0;
-    // private double maxRange = 3.0;
 
-
+    public float movementSpeed = 1f; // Speed of movement of shelf
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +20,26 @@ public class HeightSlider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(
-                originalPos.x,
-                distanceSlider.value, // It's actually directly mapped
-                originalPos.z);
-        this.transform.position = newPos;
 
-        // distance = Vector3.Distance(refObj.transform.position, this.transform.position);
+        Vector3 addedPos = new Vector3(
+                0,
+                distanceSlider.value, // It's actually directly mapped
+                0);
+
+        Vector3 maxPos = new Vector3(
+                originalPos.x,
+                3.75f,
+                originalPos.z);
+        Vector3 minPos = new Vector3(
+                originalPos.x,
+                0.25f,
+                originalPos.z);
+
+        Vector3 nextPos = this.transform.position + addedPos * movementSpeed * Time.deltaTime;
+
+        if (nextPos.y < 3.75 && nextPos.y > 0.25)
+        {
+            GetComponent<Rigidbody>().MovePosition(nextPos);
+        }
     }
 }
