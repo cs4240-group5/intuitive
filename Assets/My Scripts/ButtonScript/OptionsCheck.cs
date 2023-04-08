06149e0875c9178102d8755dcec70d2b1caf0180
DESKTOP-4RG4IVB\ruixi <ruixiong8@hotmail.com>
2023-04-08 12:49:33 +0800

    update sounds

--- Assets/My Scripts/ButtonScript/OptionsCheck.cs
+++ Assets/My Scripts/ButtonScript/OptionsCheck.cs
@@ -11,7 +11,9 @@ public class OptionsCheck : MonoBehaviour
     public GameObject checkButton;
     public GameObject moveonButton;
     public Text currentText;
-    public AudioSource audioPlayer;
+    public AudioSource correctAudio;
+    public AudioSource wrongAudio;
+
 
     public void Start()
     {
@@ -20,17 +22,19 @@ public class OptionsCheck : MonoBehaviour
     // Update is called once per frame
     public void CheckAnswer()
     {
-        audioPlayer.Play();
         if (slider.value < 4)
-        {
+        {   
+            wrongAudio.Play();
             currentText.text = "Too Little! \nTry again.";
         }
         else if (slider.value > 6)
         {
+            wrongAudio.Play();
             currentText.text = "Too Many! \nTry again.";
         }
         else
-        {
+        {   
+            correctAudio.Play();
             currentText.text = "Nice answer! \nClick to move on.";
             checkButton.transform.localScale = Vector2.zero;
             moveonButton.transform.localScale = Vector2.one;
