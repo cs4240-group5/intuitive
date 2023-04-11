cd5ae8b3e114e49ea66304b9ee9d9e262ab9b631
DESKTOP-4RG4IVB\ruixi <ruixiong8@hotmail.com>
2023-04-11 15:51:19 +0800

    remove slider for options

--- Assets/My Scripts/ButtonScript/MoveToNext2.cs
+++ Assets/My Scripts/ButtonScript/MoveToNext2.cs
@@ -11,13 +11,15 @@ public class MoveToNext2 : MonoBehaviour
     public GameObject currentPanel;
     public GameObject newPanel;
     public AudioSource audioPlayer;
-    public GameObject optionsSlider;
     public GameObject tipsSlider;
 
     public GameObject tipsPanel;
     public GameObject miniGamePanel;
     public GameObject explanationPanel;
     public GameObject selectionPanel;
+
+    public GameObject increment;
+    public GameObject decrement;
 
     void Start()
     {
@@ -37,7 +39,6 @@ public class MoveToNext2 : MonoBehaviour
     {
         currentPanel.transform.localScale = Vector2.zero;
         newPanel.transform.LeanScale(Vector2.one, 0.3f);
-        optionsSlider.transform.localScale = Vector2.zero;
         tipsSlider.transform.localScale = new Vector2(2.5f, 2.5f);
         audioPlayer.Play();
 
@@ -46,6 +47,8 @@ public class MoveToNext2 : MonoBehaviour
         selectionPanel.LeanScale(Vector2.one, 0.3f);
         rightPanel.transform.localScale = Vector2.zero;
         panel2UpPanel.transform.localScale = Vector2.one;
+        decrement.transform.localScale = Vector2.zero;
+        increment.transform.localScale = Vector2.zero;
         textBoard.text = "Hick's Law";
         //textBoard.transform.localScale = Vector2.zero;
     //tipsPanel.LeanScale(Vector2.zero, 0.3f);
